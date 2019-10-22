75fb0069a2536172529932aa97e6f64f9ca0d69f
Stephanie Mach <smach8999@yahoo.com>
2019-10-21 19:07:41 -0500

    Add UI

--- dist/js/main.js
+++ dist/js/main.js
@@ -10598,19 +10598,56 @@ return jQuery;
 } );
 
 var apod = {
-    // Application Constructor
-    init: function() {
+      //Create a random date
+      randomDate: function(start, end) {
+        //Randomize the date https://gist.github.com/miguelmota/5b67e03845d840c949c4
+        let date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  
+        //Format the date
+        let d = date.getDate();
+        let m = date.getMonth() + 1; //In JS months start at 0
+        let y = date.getFullYear();
+  
+        //Change the maonth and day strings so that they match the documented format.
+        if(m < 10){
+          m = '0'+m
+        }
+  
+        if(d < 10){
+          d = '0'+d
+        }
+  
+        return `${y}-${m}-${d}`;
+      },
 
-        var url = "https://api.nasa.gov/planetary/apod?api_key=OxuefVhPLvZLiYQc9K7MMo5ZNqPOMvas4yGZqlUf";
+      
+  // Application Constructor
+  init: function() {
+      let date = "2013-06-06";
+      //  let date = this.randomDate(new Date(1995, 5, 16), new Date());
+      var url = "https://api.nasa.gov/planetary/apod?api_key=390M91Et3zMGVAsL2q7BYziWdK5Q3lfIMhFoSRl2&date=" + date;
+     //  var url = "https://api.nasa.gov/planetary/apod?api_key=C4Ttiwi0wMTc23wYTD1ntCausYof3r953Vj4ro34";
+      //  var url = "https://api.nasa.gov/planetary/apod?api_key=OxuefVhPLvZLiYQc9K7MMo5ZNqPOMvas4yGZqlUf=" + date;
 
-        $.ajax({
-            url: url
-        }).done(function(result){
-          console.log(result);
-        }).fail(function(result){
-          console.log(result);
-        });
-    },
+    $.ajax({
+      url: url
+  })
+  // .done(function(result){
+  //   console.log(result);
+  // }).fail(function(result){
+  //   console.log(result);
+  // });
+
+  .done(function(result){
+    $("#apodTitle").text(result.title);
+    $("#apodImg").attr("src", result.url).attr('alt', result.title);
+    $("#apodCopyright").text("Copyright: " + result.copyright);
+    $("#apodDate").text("Date: " + date);
+    $("#apodDesc").text(result.explanation);
+    console.log(result);
+  });
+
+},
 };
 
 apod.init();
